a95e1f8f2b2781fdb757e2925d2828b133f9fc53
Juan Esteban Guevara <Juan.Guevara@WLAPCOING94.local>
2024-12-19 12:30:30 -0500

    feat(SYSTICK): Implemented led blink logic

--- 4100901-ClassWork/Src/systick.c
+++ 4100901-ClassWork/Src/systick.c
@@ -1,4 +1,6 @@
 #include "systick.h"
+#include "gpio.h"
+#include "uart.h"
 
 typedef struct {
     volatile uint32_t CTRL;
@@ -8,11 +10,14 @@ typedef struct {
 
 } SysTick_t;
 
-
 #define SysTick ((SysTick_t *)0xE000E010) // Base address of SysTick
 
 
 volatile uint32_t ms_counter = 0; // Counter for milliseconds
+extern int LED_LIGHT_MASK;
+extern int INFINITE_BLINK;
+int led_state=0;
+int counter=0;
 
 
 void configure_systick_and_start(void)
@@ -34,5 +39,22 @@ void systick_reset(void)
 
 void SysTick_Handler(void)
 {
-    ms_counter++;
+    ms_counter = (ms_counter + 1) % (100*BLINK_PERIOD_MS+1);
+    if(ms_counter%(BLINK_PERIOD_MS/2) == 0){
+        if(led_state==0){
+            led_state = 7;
+        }
+        else {
+            led_state = 0;
+        }
+        int temporal_ouptut = led_state & (2*LED_LIGHT_MASK+1);
+        GPIOA->ODR = (temporal_ouptut)<<5;
+        if(temporal_ouptut & 6 && LED_LIGHT_MASK != 3){
+            counter = (counter + 1)%3;
+            if (counter==0 && !(INFINITE_BLINK & LED_LIGHT_MASK)){
+                LED_LIGHT_MASK=0;
+                INFINITE_BLINK=0;
+            }
+        }
+    }
 }
